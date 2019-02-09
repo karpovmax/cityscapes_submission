968e8b6b66d13a83e5f260bad288fabdd18ea5cc
Maxim Karpov <max6482@yandex.ru>
2019-02-09 14:22:15 +0300

    Update model.py

--- model.py
+++ model.py
@@ -167,7 +167,7 @@ with tf.device('device:GPU:1'):
     config = tf.ConfigProto()
     config.gpu_options.allow_growth = True
     with tf.Session(config=config) as sess:
-        model = load_model('unet_140epochs.hdf5', custom_objects={'tversky_loss': tversky_loss})
+        model = load_model('program/unet_140epochs.hdf5', custom_objects={'tversky_loss': tversky_loss})
         sess.run(tf.global_variables_initializer())
         pred = model.predict(x_test, verbose=0)
         pred = np.argmax(pred,axis=3).astype(int)
